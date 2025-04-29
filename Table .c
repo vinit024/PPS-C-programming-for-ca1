06ec41f0e5bf95700d43827552d3ed306099917e
vinit024 <vinitsontakke96@gmail.com>
2025-04-29 21:18:03 +0530

    Add files via upload

---
+++ Table .c
@@ -0,0 +1,9 @@
+// Name:Vinit Sontakke PRN:24030331378024
+#include <stdio.h>
+int main() {
+    int i;
+    for(i = 1; i <= 10; i++) {
+        printf("10 x %d = %d\n", i, 10 * i);
+    }
+    return 0;
+}
